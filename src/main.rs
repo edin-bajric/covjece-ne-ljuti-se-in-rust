aa9c8a0914ad4745d30b97566abbe069a1c628f5
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-15 19:09:19 +0200

    added a function which prints the logo

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,24 @@ extern crate rand;
 
 use rand::Rng;
 
+fn print_logo() {
+    let logo = r#"
+     _               _       
+    | |             | |      
+    | |    _   _  __| | ___  
+    | |   | | | |/ _` |/ _ \ 
+    | |___| |_| | (_| | (_) |
+    |______\__,_|\__,_|\___/ 
+
+    "#;
+    
+    println!("{}", logo);
+}
+
 fn roll() -> u32 {
     let mut rng = rand::thread_rng();
     rng.gen_range(1..=6)
 }
 fn main() {
-    
+    print_logo();
 }
