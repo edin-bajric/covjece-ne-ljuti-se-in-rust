3b2f13d54887da6afaa325a599d6f65cb9a760bf
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-12-29 19:38:06 +0100

    added functions which handle regular turns after initial 3 turns

--- src/main.rs
+++ src/main.rs
@@ -117,6 +117,95 @@ fn initial_rolls(players: &mut Vec<Player>) {
     }
 }
 
+fn move_existing_pawn(player: &mut Player, roll_result: u32) {
+    let empty_count = player.pawns.iter().filter(|&&pawn| pawn == 0).count();
+
+    if empty_count == 3 {
+        if let Some(non_empty_index) = player.pawns.iter().position(|&pawn| pawn != 0) {
+            player.pawns[non_empty_index] += roll_result;
+        }
+    } else if empty_count < 3 {
+        loop {
+            println!("Choose which pawn to move (1, 2, 3, or 4): ");
+            println!("pawn positions on the board - {:?}", player.pawns);
+            let mut pawn_number_input = String::new();
+            io::stdin().read_line(&mut pawn_number_input).expect("Failed to read line");
+            let pawn_number: usize = match pawn_number_input.trim().parse() {
+                Ok(parsed) => parsed,
+                Err(_) => {
+                    println!("Invalid input. Please enter a number between 1 and 4.");
+                    continue;
+                }
+            };
+
+            if pawn_number >= 1 && pawn_number <= 4 && player.pawns[pawn_number - 1] != 0 {
+                player.pawns[pawn_number - 1] += roll_result;
+                break;
+            } else {
+                println!("Invalid pawn number or pawn already at the target position. Try again.");
+            }
+        }
+    }
+}
+
+fn place_new_pawn(player: &mut Player) {
+    if let Some(empty_index) = player.pawns.iter().position(|&pawn| pawn == 0) {
+        player.pawns[empty_index] = 1;
+    } else {
+        println!("Player has no empty space for a new pawn.");
+    }
+}
+
+fn turns(players: &mut Vec<Player>) {
+    let mut current_player = 0;
+
+    loop {
+        let player = &mut players[current_player];
+        let roll_result = player.roll();
+        println!("{} rolled: {}", player.color, roll_result);
+
+        if roll_result == 6 && player.pawns[0] == 0 {
+            player.pawns[0] = 1;
+        } else if roll_result == 6 {
+            loop {
+                println!("Press 'm' to move an existing pawn or 'p' to place another pawn on the board");
+                let mut action = String::new();
+                io::stdin().read_line(&mut action).expect("Failed to read line");
+                action = action.trim().to_lowercase();
+
+                match action.as_str() {
+                    "m" => {
+                        move_existing_pawn(player, roll_result);
+                        break;
+                    }
+                    "p" => {
+                        place_new_pawn(player);
+                        break;
+                    }
+                    _ => {
+                        println!("Invalid option. Please enter 'm' or 'p'.");
+                    }
+                }
+            }
+        } else {
+            let empty_count = player.pawns.iter().filter(|&&pawn| pawn == 0).count();
+            if empty_count == 4 {
+                current_player = (current_player + 1) % players.len();
+                continue;
+            } else if empty_count == 3 {
+                if let Some(non_empty_index) = player.pawns.iter().position(|&pawn| pawn != 0) {
+                    player.pawns[non_empty_index] += roll_result;
+                }
+            } else if empty_count < 3 {
+                move_existing_pawn(player, roll_result);
+            }
+        }
+
+        println!("pawn positions on the board - {:?}", player.pawns);
+        current_player = (current_player + 1) % players.len();
+    }
+}
+
 fn run_game() {
     clearscreen::clear().expect("failed to clear screen");
     print_logo();
@@ -129,6 +218,7 @@ fn run_game() {
     }
 
     initial_rolls(&mut players);
+    turns(&mut players);
 }
 
 fn main() {
